17437971c8145635c75f06027a78c5a274a6b9f4
Gauri Iyer <gauri.iyer@galvanize.com>
2023-03-23 14:11:53 -0700

    init RTL methods

--- src/tests/counter.test.js
+++ src/tests/counter.test.js
@@ -1,7 +1,20 @@
-import { render } from '@testing-library/react'
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
+
+import Counter from '../components/counter.jsx'
 
 describe('renders all pieces to frontend', () => {
+
   it('should render the counter button', () => {
-    render();
+    render(<Counter />);
+    expect(screen.queryByRole('button', { name: "increment" })).toBeTruthy();
+  })
+
+  it('should increment the value on click', () => {
+    render(<Counter />);
+    expect(screen.queryByText('counter: 0')).toBeTruthy();
+    fireEvent.click(screen.getByRole('button'));
+    expect(screen.queryByText('counter: 0')).toBeFalsy();
+    expect(screen.queryByText('counter: 1')).toBeTruthy();
   })
 })
